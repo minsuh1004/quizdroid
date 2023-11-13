753543b85b522bc7ebafadd69f74907c22b00047
minsuh1004 <immsk812@gmail.com>
2023-11-12 17:53:19 -0800

    Modified file path for JSON

--- app/src/main/java/edu/uw/ischool/msk812/quizdroid/QuizApp.kt
+++ app/src/main/java/edu/uw/ischool/msk812/quizdroid/QuizApp.kt
@@ -1,13 +1,16 @@
 package edu.uw.ischool.msk812.quizdroid
 
 import android.app.Application
+import android.content.Context
+import android.os.Environment
+import android.util.JsonReader
 import android.util.Log
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import java.io.File
 import java.io.FileReader
-
-
+import java.io.FileWriter
+import kotlin.io.path.Path
 
 
 data class Quiz(val text: String, val answer: Int, val answers: List<String>)
@@ -19,9 +22,9 @@ interface TopicRepository {
     fun getQuiz(topicNum: Int, index: Int) : Quiz
 }
 
-class MockTopicRepository : TopicRepository {
-
-    val readFile: String = FileReader("/data/local/tmp/questions.json").readText()
+class MockTopicRepository(context: Context) : TopicRepository {
+    val readFile = File(context.filesDir, "questions.json").readText()
+    //val readFile: String = FileReader("/data/user/0/edu.uw.ischool.msk812.quizdroid/files/questions.json").readText()
     val fileData: List<Topic> = Gson().fromJson(readFile, object : TypeToken<List<Topic>>() {}.type)
     override fun getAllTopics(): List<Topic> {
         val topicsList : MutableList<Topic> = mutableListOf()
@@ -59,7 +62,8 @@ class QuizApp : Application() {
         super.onCreate()
         Log.i("QuizApp", "onCreate() loaded and running")
         instance = this
+        Log.i("TopicRepo", "filesDir = $filesDir")
 
-        topicRepository = MockTopicRepository()
+        topicRepository = MockTopicRepository(applicationContext)
     }
 }
